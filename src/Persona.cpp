20ed2883eb8d917284ecd5b5758ca8c1c8804a4a
Jaal-09 <jaal9709+github@gmail.com>
2026-05-16 17:32:29 -0500

    Agregados los Getters de la clase principal Persona

--- src/Persona.cpp
+++ src/Persona.cpp
@@ -18,3 +18,27 @@ using namespace std;
     this->documento = documento;
  }
 
+ //destructor
+ Persona::~Persona(){
+    //Es vital es importante para la herencia
+ }
+
+ //Getters
+string Persona::getNombre() const{
+    return this->nombre;
+}
+
+string Persona::getApellido() const{
+    return this->apellido;
+}
+
+int Persona::getEdad() const{
+    return this->edad;
+}
+
+string Persona::getDocumento() const{
+    return this->documento;
+}
+
+
+

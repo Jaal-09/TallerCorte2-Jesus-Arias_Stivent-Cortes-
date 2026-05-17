5992ff4e4681fdc158e31215f011ddc81bb953b5
Jaal-09 <jaal9709+github@gmail.com>
2026-05-16 19:43:39 -0500

    Creado el archivo de la implementacion de la clase persona y agregado el constructor

---
+++ src/Profesor.cpp
@@ -0,0 +1,10 @@
+#include "Profesor.h"
+#include "Curso.h"
+
+using namespace std;
+
+/**
+ * @brief Constructor de profesor
+ * 
+ * Llama al constructor de Persona y luego inicializa los atributos propios
+ */

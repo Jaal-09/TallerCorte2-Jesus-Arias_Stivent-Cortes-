078d089406ac09dc514604f75d50443cdc27e996
Jaal-09 <jaal9709+github@gmail.com>
2026-05-17 12:16:57 -0500

    Agregados Getters, Setters y metodos de la clase Matricula

--- src/Matricula.cpp
+++ src/Matricula.cpp
@@ -16,3 +16,43 @@ Matricula::Matricula(Estudiante* estudiante, Curso* curso, string estado){
 Matricula::~Matricula(){
     //Aunque no liberamos estudiante ni curso, son gestionados por el sistema
 }
+
+//Getters
+Estudiante* Matricula::getEstudiante() const{
+    return this->estudiante;
+}
+
+Curso* Matricula::getCurso() const {
+    return this->curso;
+}
+
+string Matricula::getEstado() const{
+    return this->estado;
+}
+
+double Matricula::getNotaFinal() const{
+    return this->notaFinal;
+}
+
+//Setters
+void Matricula::setEstado(string estado){
+    this->estado = estado;
+}
+
+void Matricula::asignarNota(double nota){
+    //Validación de nota
+    if(nota >= 0.0 && nota <= 5.0){
+        this->notaFinal = nota;
+        cout << " Nota: " << nota << " asignada a " << estudiante->getNombre() << " " << estudiante->getApellido() << " en el curso " << curso->getNombreCurso() << endl;
+
+        //Si la nota es aprobatoria cambiamos el estado
+        if(nota >= 3.0 && estado == "Activa"){
+            setEstado("Aprobada");
+        } else if( nota < 3.0 && estado == "Activa"){
+            setEstado("Reprobada");
+        }
+    }else{
+        cerr << "Error: Nota no válida (" << nota << "). Debe estar entre 0.0 y 5.0" << endl;
+    }
+}
+

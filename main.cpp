d17d5ae6a3faf81919503e4e00d676da7271465c
Jaal-09 <jaal9709+github@gmail.com>
2026-05-17 14:18:40 -0500

    Menu del proyecto terminado

--- main.cpp
+++ main.cpp
@@ -18,3 +18,43 @@
 #include "Profesor.h"
 #include "Curso.h"
 #include "Matricula.h"
+
+using namespace std;
+
+int main(){
+
+    //Vectores para almacenar datos
+    vector<Estudiante*> estudiantes;
+    vector<Profesor*> profesores;
+    vector<Curso*> cursos;
+    vector<Matricula*> matriculas;
+
+    int opcion;
+
+    cout << "       SISTEMA DE GESTIÓN UNIVERSITARIA        " << endl;
+
+    do{
+        mostrarMenu();
+        cout << "Seleccione: ";
+        cin >> opcion;
+
+        switch(opcion){
+            case 1: registrarEstudiante(estudiantes); break;
+            case 2: registrarProfesor(profesores); break;
+            case 3: crearCurso(cursos, profesores); break;
+            case 4: matricularEstudiante(matriculas, estudiantes, cursos); break;
+            case 5: asignarNota(matriculas); break;
+            case 6: mostrarEstudiantes(estudiantes); break;
+            case 7: mostrarProfesores(profesores); break;
+            case 8: mostrarCursos(cursos); break;
+            case 9: mostrarMatriculas(matriculas); break;
+            case 10: cout << "\n Hasta Pronto " << endl; break;
+            default: cout << "Opcion incorrecta. " << endl;
+        }
+    }while(opcion != 10);
+
+    //Liberar la memoria
+    liberarMemoria(estudiantes, profesores, cursos, matriculas);
+
+    return 0;
+}

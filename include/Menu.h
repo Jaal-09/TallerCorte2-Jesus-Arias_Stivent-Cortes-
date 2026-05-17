74d75255e537b452fb7e8ebd1dcd96a1399bca9e
Jaal-09 <jaal9709+github@gmail.com>
2026-05-17 12:49:13 -0500

    Agregados metodos para mostrar profesores, mostrar estudiantes en la cabecera del menu

--- include/Menu.h
+++ include/Menu.h
@@ -13,3 +13,56 @@ class Matricula;
  * @brief Muestra el menú principal en la consola
  * 
  */
+void mostrarMenu();
+
+/**
+ * @brief Registrar un nuevo estudiante
+ * @param estudiantes Vector de punteros a Estudiante
+ */
+void registarEstudiante(std::vector<Estudiante*>& estudiantes);
+
+/**
+ * @brief Registra un nuevo profesor
+ * @param profesores Vector de punteros a Profesor
+ * 
+ */
+void registrarProfesor(std::vector<Profesor*>& profesores);
+
+/**
+ * @brief Crea un nuevo curso
+ * @param cursos Vector de punteros a Curso
+ * @param profesores Vector de profesores (para asignar)
+ */
+void crearCurso(std::vector<Curso*>& cursos, std::vector<Profesor*>& profesores);
+
+/**
+ * @brief Matricula un estudiante en un curso
+ * @param matriculas Vector de punteros a Matricula
+ * @param estudiantes Vector de estudiantes
+ * @param cursos Vector de cursos
+ */
+void matricularEstudiante(std::vector<Matricula*>& matriculas, std::vector<Estudiante*>& estudiantes, std::vector<Curso*>& cursos);
+
+/**
+ * @brief Asigna una nota a la matricula
+ * @param matriculas Vector de matriculas
+ * 
+ */
+void asignarNota(std::vector<Matricula*>& matriculas);
+
+/**
+ * @brief Muestra todos los estudiantes
+ * @param estudiantes Vector de estudiantes
+ */
+void mostrarEstudiantes(const std::vector<Estudiante*>& estudiantes);
+
+/**
+ * @brief Muestra todos los profesores
+ * @param profesores Vector de profesores
+ */
+void mostrarProfesores(const std::vector<Profesor*>& cursos);
+
+/**
+ * @brief 
+ * 
+ */

0377f0921131b297b47db8fa5296b0e775e5056f
Jaal-09 <jaal9709+github@gmail.com>
2026-05-17 11:38:30 -0500

    Agregados los metodos de la clase Curso, terminada la implementación de esta clase

--- src/Curso.cpp
+++ src/Curso.cpp
@@ -51,3 +51,56 @@ void Curso::setCreditos(int creditos){
         cerr << "Error: Créditos no válidos (" << creditos << "). Deben estar entre 1 y 6." << endl;
     }
 }
+
+/**
+ * @brief Metodos para registar profesor y estudiante
+ * 
+ * @param prof 
+ */
+
+//Registrar Profesor
+void Curso::registrarProfesor(Profesor* prof){
+    if(prof != nullptr){
+        this->profesor = prof;
+        cout << "Profesor " << prof->getNombre() << " " << prof->getApellido() << " registrado en el curso " << this->nombreCurso << endl;
+    } else{
+        cerr << "Error: Profesor no válido" << endl;
+    }
+}
+
+//Registrar Estudiante
+void Curso::registrarEstudiante(Estudiante* est){
+    if(est != nullptr){
+        estudiantes.push_back(est);
+        cout << "Estudiante " << est->getNombre() << " " << est->getApellido() << " registrado en el curso " << this->nombreCurso << endl;
+    }
+}
+
+//Mostrar información del curso
+void Curso::mostrarInformacion() const{
+    cout << "       Información del Curso       " << endl;
+    cout << " Nombre:       " << this->nombreCurso << endl;
+    cout << " Código:       " << this->codigoCurso << endl;
+    cout << " Creditos:     " << this->creditos << endl;
+
+    if(profesor != nullptr){
+        cout << " Profesor:  " << profesor->getNombre() << " " << profesor->getApellido() << endl;
+    } else{
+        cout << " Estudiantes: " << estudiantes.size() << " inscritos" << endl;
+        cout << "_______________________________________" << endl;
+    }
+}
+
+//Mostrar lista de estudiantes
+void Curso::mostrarEstudiantes() const{
+    if(estudiantes.empty()){
+        cout << "No hay estudiantes inscritos en el curso " << this->nombreCurso << endl;
+        return;
+    }
+
+    cout << "\n --- Estudiantes inscritos en " << this->nombreCurso << " ---" << endl;
+    for(size_t i = 0; i < estudiantes.size(); i++){
+        cout << i + 1 << ". " << estudiantes[i]->getNombre() << " " << estudiantes[i]->getApellido() << " (código: " << estudiantes[i]->getCodigo() << ")" << endl;
+    }
+}
+

6925221fe54be8b8b580febc7a57a97d14e461c8
Jaal-09 <jaal9709+github@gmail.com>
2026-05-16 19:51:30 -0500

    Agregados los Getters y Setters de la clase Profesor

--- src/Profesor.cpp
+++ src/Profesor.cpp
@@ -8,3 +8,36 @@ using namespace std;
  * 
  * Llama al constructor de Persona y luego inicializa los atributos propios
  */
+
+ Profesor::Profesor(string nombre, string apellido, int edad, string documento, string especialidad, double salario) : Persona(nombre, apellido, edad, documento){
+    this->especialidad = especialidad;
+    this->salario = salario;
+ }
+
+ //Destructor
+ Profesor::~Profesor(){
+    //Nada que liberar
+ }
+
+//Getters
+string Profesor::getEspecialidad() const{
+    return this->especialidad;
+}
+
+double Profesor::getSalario() const {
+    return this->salario;
+}
+
+//Setters
+void Profesor::setEspecialidad(string especialidad){
+    this->especialidad = especialidad;
+}
+
+void Profesor::setSalario(double salario){
+    //Validacion para el salario
+    if(salario > 0){
+        this->salario = salario;
+    } else{
+        cerr << "Error: Slario no válido (" << salario << "). Debe ser mayor a 0." << endl;
+    }
+}

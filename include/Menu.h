dce9607474bf6100099ec60f63932621b7ee12ac
Jaal-09 <jaal9709+github@gmail.com>
2026-05-17 12:30:36 -0500

    Empezando la cabecera del menu del proyecto

---
+++ include/Menu.h
@@ -0,0 +1,15 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <vector>
+
+//Declaracion anticipada de las clases
+class Estudiante;
+class Profesor;
+class Curso;
+class Matricula;
+
+/**
+ * @brief Muestra el menú principal en la consola
+ * 
+ */

356bd39f983b458cc474b97938e52f88f05b0ab1
Jaal-09 <jaal9709+github@gmail.com>
2026-05-17 14:02:39 -0500

    Creando el punto de entrada del proyecto main

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief Punto de entrada del sistema de registro universitario
+ * @version 1.0
+ * @date 2026-05-17
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
+#include <iostream>
+#include <vector>
+
+//Includes del proyectp
+#include "Menu.h"
+#include "Estudiante.h"
+#include "Profesor.h"
+#include "Curso.h"
+#include "Matricula.h"

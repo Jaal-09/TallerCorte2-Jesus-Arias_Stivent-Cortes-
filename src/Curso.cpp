ab95df70aa7dc085f00fad399bdb9a20bae1920b
Jaal-09 <jaal9709+github@gmail.com>
2026-05-17 11:17:48 -0500

    Agregados Getters y Setters de la Clase Curso

--- src/Curso.cpp
+++ src/Curso.cpp
@@ -17,3 +17,37 @@ Curso::~Curso(){
     estudiantes.clear();
 }
 
+//Getters
+string Curso::getNombreCurso() const{
+    return this->nombreCurso;
+}
+
+string Curso::getCodigoCurso() const{
+    return this->codigoCurso;
+}
+
+int Curso::getCreditos() const{
+    return this->creditos;
+}
+
+Profesor* Curso::getProfesor() const{
+    return this->profesor;
+}
+
+//Setters
+void Curso::setNombreCurso(string nombreCurso){
+    this->nombreCurso = nombreCurso;
+}
+
+void Curso::setCodigoCurso(string codigoCurso){
+    this->codigoCurso = codigoCurso;
+}
+
+void Curso::setCreditos(int creditos){
+    //Validacion de los creditos
+    if(creditos > 0 && creditos <= 6){
+        this->creditos = creditos;
+    } else{
+        cerr << "Error: Créditos no válidos (" << creditos << "). Deben estar entre 1 y 6." << endl;
+    }
+}
